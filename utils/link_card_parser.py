a9b235cc3219ae1de84ce62b0b3513925d5521b8
BrenDev0 <lahey1991@gmail.com>
2025-05-06 15:46:13 -0600

    awaiting invoke

--- utils/link_card_parser.py
+++ utils/link_card_parser.py
@@ -75,7 +75,7 @@ class LinkCardParser:
         for row in rows:
             try:
                 input = {'input': row}
-                response = chain.invoke(input) 
+                response = await chain.invoke(input) 
                 output = ast.literal_eval(response.content)
                 if output.get("Nombre") is None or output.get("Telefono") is None:
                     raise ValueError("Nombre y Teléfono son campos obligatorios.")
